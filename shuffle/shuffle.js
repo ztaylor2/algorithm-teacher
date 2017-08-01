05362c133f7f6a8373bc79aecc2c084dfb8cfd4e
jensjoh01 <johnnyj2439@gmail.com>
2017-08-01 12:59:55 -0700

    step through and play working

--- shuffle/shuffle.js
+++ shuffle/shuffle.js
@@ -3,13 +3,13 @@
 var cardValue = ['2','3','4','5','6','7','8','9','10','jack','queen','king','ace'];
 var cardSuit = ['clubs','hearts','spades','diamonds'];
 var deck = [];
-var deck0 = [];
-var randomIndexArray = [];
 var leftCardPosition = [10,100,190,280,370,460,550,640,730,820,910,1000,1090,10,100,190,280,370,460,550,640,730,820,910,1000,1090,10,100,190,280,370,460,550,640,730,820,910,1000,1090,10,100,190,280,370,460,550,640,730,820,910,1000,1090];
-var topCardPositionRow1 = 40;
-var topCardPositionRow2 = 160;
-var topCardPositionRow3 = 280;
-var topCardPositionRow4 = 400;
+var topCardPositionRow1 = 80;
+var topCardPositionRow2 = 205;
+var topCardPositionRow3 = 330;
+var topCardPositionRow4 = 455;
+var currentIndex;
+var pressPlay = false;
 
 function buildDeck(){
   for(var i = 0; i < cardSuit.length; i++) {
@@ -17,24 +17,34 @@ function buildDeck(){
       deck.push(cardValue[j] + '_of_' + cardSuit[i] + '.png');
     }
   }
+  currentIndex = deck.length;
 }
+
 /*Fisher-Yates shuffle based on stackoverflow https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 */
-function shuffle(array) {
-  var currentIndex = array.length;
+function shuffle(event) {
+  // event.preventDefault();
   var tempValue;
   var randomIndex;
-  var count = 0;
-
-  while (currentIndex !== 0){
+  if (currentIndex !== 0){
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
-    randomIndexArray.push(randomIndex);
-    tempValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = tempValue;
+    tempValue = deck[currentIndex];
+    deck[currentIndex] = deck[randomIndex];
+    deck[randomIndex] = tempValue;
+    displayShuffle(currentIndex, randomIndex);
+    if (pressPlay === true && currentIndex !== 0){
+      play();
+    } else if (pressPlay === true && currentIndex === 0) {
+      pressPlay = false;
+      alert('Congratulations! You\'ve shuffled the deck!');
+      currentIndex = deck.length;
+      document.getElementById('step').addEventListener('click',shuffle);
+    }
+  } else {
+    alert('Congratulations! You\'ve shuffled the deck!');
+    currentIndex = deck.length;
   }
-  return array;
 }
 
 function displayCards() {
@@ -76,19 +86,58 @@ function moveCards() {
   }
 }
 
-function highlight(){
-  document.getElementById('card5').style.transform = 'scale3d(1.4, 1.4, 1)';
-  document.getElementById('card5').style.opacity = '1';
-  document.getElementById('card5').style.zIndex = '2';
-  document.getElementById('card5').style.boxShadow = '0px 0px 100px #000000';
-  document.getElementById('card5').style.animation = 'move';
+function displayShuffle(currentIndex, randomIndex) {
+  var content = document.getElementById('cards-div');
+  while (content.hasChildNodes()){
+    content.removeChild(content.lastChild);
+  }
+  for(var i = 0; i < deck.length; i++){
+    var img = document.createElement('img');
+    img.setAttribute('src','img/' + deck[i]);
+    img.setAttribute('id', 'card' + i);
+    content.appendChild(img);
+  }
+  moveCards();
+  highlight(currentIndex, randomIndex)
+}
 
-  document.getElementById('card22').style.transform = 'scale3d(1.4, 1.4, 1)';
-  document.getElementById('card22').style.opacity = '1';
-  document.getElementById('card22').style.zIndex = '2';
-  document.getElementById('card22').style.boxShadow = '0px 0px 100px #000000';
+function highlight(currentIndex, randomIndex){
+  // event.preventDefault();//neccessary??
+  console.log('current card' + currentIndex);
+  console.log('random card' + randomIndex);
+
+  document.getElementById('card' + currentIndex).style.transform = 'scale3d(1.4, 1.4, 1)';
+  document.getElementById('card' + currentIndex).style.opacity = '1';
+  document.getElementById('card' + currentIndex).style.zIndex = '2';
+  document.getElementById('card' + currentIndex).style.boxShadow = '0px 0px 100px #000000';
+
+  document.getElementById('card' + randomIndex).style.transform = 'scale3d(1.4, 1.4, 1)';
+  document.getElementById('card' + randomIndex).style.opacity = '1';
+  document.getElementById('card' + randomIndex).style.zIndex = '2';
+  document.getElementById('card' + randomIndex).style.boxShadow = '0px 0px 100px #000000';
+}
+
+function play(){
+  document.getElementById('step').removeEventListener('click',shuffle);
+  pressPlay = true;
+  if (currentIndex !== 0){
+    var myTimeout = setTimeout(shuffle, 500);
+  }
+  document.getElementById('play').innerHTML = 'Stop';
+  document.getElementById('play').removeEventListener('click',play);
+  document.getElementById('play').addEventListener('click',stop);
+}
+
+function stop() {
+  pressPlay = false;
+  document.getElementById('step').addEventListener('click',shuffle);
+  document.getElementById('play').removeEventListener('click',stop);
+  document.getElementById('play').innerHTML = 'Play';
+  document.getElementById('play').addEventListener('click',play);
+  console.log('????');
 }
 
 displayCards();
-highlight();
-document.getElementById('step').addEventListener('click',)
+
+document.getElementById('step').addEventListener('click',shuffle);
+document.getElementById('play').addEventListener('click',play);
