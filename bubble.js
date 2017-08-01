f749d61db6d0da45b8f29e558a0a77a6cbe4c029
Zach Taylor <ztaylor2@zagmail.gonzaga.edu>
2017-08-01 10:10:45 -0700

    added descriptions

--- bubble.js
+++ bubble.js
@@ -6,7 +6,12 @@ var playButton = document.getElementById('playButton');
 
 var boxes = document.getElementById('boxesContainer').children;
 var nums = document.getElementById('numberInputs').children;
+var description = document.getElementById('description');
+var descriptionTextContent = [];
 
+descriptionTextContent.push('Next, it will compaire the next two numbers in the array at [1] and [2].  If the number at [2] is greater, the numbers will swap.  If not, they will remain where they are. ');
+descriptionTextContent.push('Third, it will compaire the numbers at index [2] and [3] and swap them if necessary. ');
+descriptionTextContent.push('Now, it will compaire the numbers at [3] and [4], and swap them if necessary. ');
 
 // event handler
 function sortNums(e) {
@@ -27,6 +32,8 @@ function nextStep(e) {
     boxes[loopIndex + 1].height = temp;
     console.log(loopIndex, 'still in loop');
   }
+
+  description.textContent = descriptionTextContent[loopIndex];
   loopIndex++;
 
   if (loopIndex === boxes.length - 1) {
