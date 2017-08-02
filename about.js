77a4351a1813f2f5443282ed73db9b9b60c56d54
daniel <dpillay03@gmail.com>
2017-08-02 09:50:19 -0700

    added event listeners

--- about.js
+++ about.js
@@ -1,10 +1,21 @@
 'use strict';
 
-// var zach = document.getElementById('zach');
-// var ivette = document.getElementById('ivette');
-// var john = document.getElementById('john');
-// var daniel = document.getElementById('daniel');
+document.getElementById('zach').addEventListener('click', function clickZach(e){
+  console.log("click Zach")
+  alert("");
+});
 
-document.getElementById('zack').addEventListener('click', function clickZach(){
-  console.log('click');
+document.getElementById('ivette').addEventListener('click', function clickIvette(e){
+  console.log("click Ivette")
+  alert("");
+});
+
+document.getElementById('john').addEventListener('click', function clickJohn(e){
+  console.log("click John")
+  alert("");
+});
+
+document.getElementById('daniel').addEventListener('click', function clickDaniel(e){
+  console.log("click Daniel")
+  alert("Hey guys! My name is Daniel Pillay. I was born and raised in Mountlake Terrace, WA and I am an Air Force veteran. I am glad we could help you guys learn algorithms!");
 });
