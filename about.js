932750027bd58a40691241127a39e982b8f6f33e
daniel <dpillay03@gmail.com>
2017-08-02 08:54:53 -0700

    morning merge party

--- about.js
+++ about.js
@@ -1,6 +1,10 @@
 'use strict';
 
-var zach = document.getElementById('zach');
-var ivette = document.getElementById('ivette');
-var john = document.getElementById('john');
-var daniel = document.getElementById('daniel');
+// var zach = document.getElementById('zach');
+// var ivette = document.getElementById('ivette');
+// var john = document.getElementById('john');
+// var daniel = document.getElementById('daniel');
+
+document.getElementById('zack').addEventListener('click', function clickZach(){
+  console.log('click');
+});
