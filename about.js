9823ae50ab4156650a4a6ca5eeb71bb8409ff2b1
daniel <dpillay03@gmail.com>
2017-08-02 16:08:33 -0700

    trying to fix js file. merge party is happening at 1408

--- about.js
+++ about.js
@@ -2,19 +2,15 @@
 
 document.getElementById('zach').addEventListener('click', function clickZach(e){
   console.log('click Zach');
-  alert('');
 });
-
 document.getElementById('ivette').addEventListener('click', function clickIvette(e){
   console.log('click Ivette');
   alert('Hey there! My name is Ivette.  I studied improv comedy at Second City, taught middle school and am now coding coding coding.  Come by, say "Hi", I love meeting new people tada!');
 });
-
 document.getElementById('john').addEventListener('click', function clickJohn(e){
   console.log('click John');
   alert('');
 });
-
 document.getElementById('daniel').addEventListener('click', function clickDaniel(e){
   console.log('click Daniel');
   alert('Hello! My name is Daniel Pillay. I was born and raised in Mountlake Terrace, WA and I am an Air Force veteran. I am glad we could help you guys learn algorithms!');
