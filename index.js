005c8da701bb1d38d6531759da73bfbf22328285
Ivette Cortez <ivette.p.cortez@gmail.com>
2017-08-02 09:51:22 -0700

    Merge party time, excellent! added JS for alert box and button functioning.

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 'use strict';
 
-function processButton(e) {
-  var buttonEl = document.createElement('button');
-  buttonEl.textContent = buttonEl;
-  section.appendChild(buttonEl);
-};
+function homeButton_Click(){
+  var personName_input =
+  alert('Welcome, enjoy learning algorithms!');
+}
 
-document.getElementById('button').addEventListener('click', handleClick);{
-
-function handleClick(e){
-    if (e.target.id === 'handleClick') {
-      alert('Welcome, ' + personalInfo + ', enjoy learning algorithms!')
-    }
-  }
+// function processButton(e) {
+//   var buttonEl = document.createElement('button');
+//   buttonEl.textContent = buttonEl;
+//   section.appendChild(buttonEl);
+// };
+//
+// document.getElementById('button').addEventListener('click', handleClick);{
+//
+// function handleClick(e){
+//     if (e.target.id === 'handleClick') {
+//       alert('Welcome, ' + personalInfo + ', enjoy learning algorithms!')
+//     }
+//   }
